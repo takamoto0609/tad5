f95c10ba831b082aedea555d98346f5c2b199070
takamoto0609 <takamoto0609@gamil.com>
2020-12-09 15:06:21 +0900

    areasコントローラーの作成とルーティング設定

---
+++ app/controllers/areas_controller.rb
@@ -0,0 +1,29 @@
+class AreasController < ApplicationController
+  def new
+    if user_signed_in?
+      @area = Area.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    area = Area.new(area_params)
+    if area.valid?
+      if Area.find_by(user_id: current_user.id) != nil
+        Area.find_by(user_id: current_user.id).destroy
+      end
+      Area.create(area_params)
+      redirect_to :root
+    else
+      @area = Area.new(field_params)
+      render :new
+    end
+  end
+
+  private
+
+  def area_params
+    params.require(:area).permit(:address, :user_id, :latitude, :longitude).merge(user_id: current_user.id)
+  end
+end
