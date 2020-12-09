d41d0cf6299a584e20e93a49b472014c8b9bd8e3
takamoto0609 <takamoto0609@gamil.com>
2020-12-09 15:11:19 +0900

    itemsコントローラーのindexアクションを編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
+    my_area
+  end
+
+  private
+
+  def my_area
+    if user_signed_in? && Area.find_by(user_id: current_user.id) != nil
+      gon.lat = Area.find_by(user_id: current_user.id).latitude
+      gon.lng = Area.find_by(user_id: current_user.id).longitude
+    else
+      gon.lat = nil
+      gon.lng = nil
+    end
   end
 end
