1ac9822bd19c5178f6af82d2ac241428427d2b15
takamoto0609 <takamoto0609@gamil.com>
2020-12-15 13:48:09 +0900

    サービス登録機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_many :items
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :wash_power
+  belongs_to :status
+
+  validates :comment, :address, :point, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
