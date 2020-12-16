ea3f002011e034c360101e79f2f34af15a9e1ef1
takamoto0609 <takamoto0609@gamil.com>
2020-12-17 06:00:28 +0900

    ユーザーにウォレットを付与し、所持ポイントを常に表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :get_wallet
+
+  private
+
+  def get_wallet
+    if user_signed_in?
+      if Wallet.find_by(user_id: current_user.id) == nil
+        new_wallet = Wallet.new
+        new_wallet.user_id = current_user.id
+        new_wallet.point = 0
+        new_wallet.save
+      end
+      @my_wallet = Wallet.find_by(user_id: current_user.id)
+    else
+    end
+  end
 end
