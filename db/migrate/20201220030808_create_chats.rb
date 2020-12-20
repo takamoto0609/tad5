c713bb3beec530bd79f5974094aaa656cd446118
takamoto0609 <takamoto0609@gamil.com>
2020-12-20 12:10:02 +0900

    chatsテーブルの作成

---
+++ db/migrate/20201220030808_create_chats.rb
@@ -0,0 +1,10 @@
+class CreateChats < ActiveRecord::Migration[6.0]
+  def change
+    create_table :chats do |t|
+      t.references :item,       null: false, foreign_key: true
+      t.references :user,       null: false, foreign_key: true
+      t.string     :message,    null: false
+      t.timestamps
+    end
+  end
+end
