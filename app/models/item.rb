ec24f19b781f5d2d82d21d944d92aea325ca36c1
takamoto0609 <takamoto0609@gamil.com>
2020-12-20 17:22:23 +0900

    ボタン配置を変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :wash_power
   belongs_to :status
 
-  validates :comment, :address, :point, presence: true
+  validates :wash_power_id, :status_id, :comment, :address, :point, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
